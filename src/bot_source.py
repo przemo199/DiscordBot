991274c6a4302bfd1390a13fda52051f6322c5a8
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2020-12-20 12:33:57 +0000

    Added command to bot that returns list of results for a given search term, or false if no terms are found

--- src/bot_source.py
+++ src/bot_source.py
@@ -1,5 +1,5 @@
 import os
-from scrapper import *
+from scrapper import scrapper
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -14,5 +14,15 @@ async def on_ready():
 
     print('It works')
 
+@client.command()
+async def get_price(ctx, *, nameOfGame):
+    values = scrapper.main(nameOfGame)
+    if values == False:
+        await ctx.send('No Games Found')
+    else:
+        to_print = ''
+        for line in values:
+            to_print+=f'Full Title: {line[0]}, Store Price: {line[1]}, Key Price: {line[2]}\n'
+        await ctx.send(to_print)
 
 client.run(BOT_TOKEN)
--- src/scrapper/scrapper.py
+++ src/scrapper/scrapper.py
@@ -3,12 +3,42 @@ import io
 import requests
 from lxml import etree
 
-url1 = 'https://gg.deals/gb/game/mars-horizon/'
+url1 = 'https://gg.deals/games/?title='
 title_xpath = '//*[@id="page"]/div[2]/div/ul/li[4]/a/span/text()'
 store_xpath = '//*[@id="game-card"]/div[1]/div/div[2]/div[2]/div/div[1]/a/div/span/span/text()'
 keyshop_xpath = '//*[@id="game-card"]/div[1]/div/div[2]/div[2]/div/div[2]/a/div/span/span/text()'
 
+gamesList_xpath = '//*[@id="games-list"]'
 
+def main(searchTerm):
+    searchUrl = url1 + searchTerm
+
+    response = requests.get(searchUrl)
+    response = response.text
+    htmlparser = etree.HTMLParser()
+    html = etree.parse(io.StringIO(response), htmlparser)
+
+    gamesParentPath = gamesList_xpath + '/div[1]'
+    gamesParent = html.xpath(gamesParentPath)
+
+    gamesFound = 'emptyProvider' not in gamesParent[0].get('class')
+    
+    if gamesFound:
+        games = html.xpath(gamesParentPath+'/div[1]')
+        numberOfGamesFound = len(games[0].getchildren())
+
+        number_of_games_to_return = numberOfGamesFound
+        if number_of_games_to_return > 5:
+            number_of_games_to_return = 5
+        
+        returnList = []
+        for x in range(number_of_games_to_return):
+            url = 'https://gg.deals' + games[0][x][0].get('href')
+            values = getprices(url)
+            returnList.append(getprices(url))
+        return returnList
+    return False
+    
 def getprices(url):
     response = requests.get(url)
     response = response.text
